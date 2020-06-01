71a78b3fbe39ab965c6a11ab904abd70f63bf18f
Anmolpreet <anmolpreet.kaur@yale.edu>
2020-06-01 15:30:59 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,25 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+tesla = Startup.new("Elon", "Tesla", "domain1")
+amazon = Startup.new("Jeff Bezos", "Amazon", "domain2")
+spacex = Startup.new("Elon", "SpaceX", "domain1")
+
+anmol = VentureCapitalist.new("Anmol", 2000000000)
+enrique = VentureCapitalist.new("Enrique", 5000000000)
+ziming = VentureCapitalist.new("Ziming", 5000000)
+
+# def initialize(startup, capitalist, type, investment)
+round1 = FundingRound.new(tesla, anmol, "Angel", -5.0 )
+round2 = FundingRound.new(amazon, enrique, "Series C", 4000000)
+round3 = FundingRound.new(tesla, ziming, "Angel", 50000000 )
+
+spacex.sign_contract(anmol, "Angel", 50000)
+spacex.sign_contract(enrique, "Angel", 7000)
+spacex.sign_contract(ziming, "Angel", 10000)
+
+ziming.offer_contract(tesla, "Seed", 500000)
+anmol.offer_contract(amazon, "Series B", 4000000)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
