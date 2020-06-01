b4c21834b91bc9929d8b049b658f8136e8dbef6f
KalebGz <kaleb.zeray@gmail.com>
2020-06-01 15:54:37 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class FundingRound
 
+  attr_reader :startup, :venture_capitalist, :investment
+  attr_accessor :type
+
+  @@all = []
+
+  def initialize(startup:, venture_capitalist:, type:, investment:)
+    
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment = investment.to_f
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def investment=(investment)
+    if investment >= 0
+      @investment = investment.to_f
+    else
+      puts "You can't invest a negative amount of money"
+    end
+  end
+
 end
+
+# fr = FundingRound.new(startup: "PiedPiper", venture_capitalist: "Bezos", type: "Angel", investment: 1200000)
+
+# binding.pry
+# 0
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,68 @@
+require 'pry'
+
 class Startup
 
+  attr_accessor :name
+  attr_reader :founder, :domain
+
+  @@all = []
+
+
+  def initialize(name:, founder:, domain:)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain:, name:)
+    @name = name
+    @domain = domain
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(name)
+    self.all.find{|startup| startup.founder == name}
+  end
+
+  def self.domains
+    self.all.map{|startup| startup.domain}
+  end
+
+  def sign_contract(venture_capitalist:, type:, investment:)
+    FundingRound.new(startup: self, venture_capitalist: venture_capitalist, type: type, investment: investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select{|fr| fr.startup == self}
+  end
+
+  def num_funding_rounds
+    funding_rounds.length
+  end
+
+  def total_funds
+    funding_rounds.map{|fr| fr.investment}.sum
+  end
+
+  def investors
+    funding_rounds.map{|fr| fr.venture_capitalist}.uniq
+  end
+
+  def big_investors
+    investors.select{|investor| investor.total_worth >= 1000000000}
+  end
+
+  private
+
+  attr_writer :domain
+
 end
+
+# newstartup = Startup.new(name: "PiedPiper", domain: "TV Show", founder: "Thomas Middleditch")
+
+# binding.pry
+# 0
