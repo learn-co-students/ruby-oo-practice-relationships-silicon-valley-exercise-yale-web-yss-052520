f7b0356408b882173fd9d79ea8c46a1cee1d74bf
edensongjbs <edensongjbs@gmail.com>
2020-06-01 16:26:53 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,17 @@
 class FundingRound
-
+    attr_reader :startup, :venture_capitalist, :type, :investment
+    @@all = []
+    def initialize(startup, venture_capitalist, type, investment)
+        @startup=startup
+        @venture_capitalist=venture_capitalist
+        @type=type
+        self.investment=investment
+        @@all << self
+    end
+    def investment=(amount)
+        @investment=(amount<0) ? 0.to_f : amount.to_f
+    end
+    def self.all
+        @@all
+    end
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,41 @@
 class Startup
-
-end
+    attr_reader :founder, :domain, :name
+    @@all = []
+    def initialize(name, founder, domain)
+      @founder = founder
+      @name = name
+      @domain = domain
+      @@all << self
+    end
+    def self.all
+      @@all
+    end
+    def pivot(domain, name)
+      @domain = domain
+      @name = name
+    end
+    def self.find_by_founder(founder_name)
+      @@all.find{|startup| startup.founder == founder_name}
+    end
+    def self.domains
+      @@all.map{|startup| startup.domain}.uniq
+    end
+    def sign_contract(vc, investment_type, amount)
+      FundingRound.new(self, vc, investment_type, amount)
+    end
+    def funding_rounds
+      FundingRound.all.select{|round| round.startup == self}
+    end
+    def num_funding_rounds
+      funding_rounds.count
+    end
+    def total_funds
+      funding_rounds.reduce(0.0){|total, round| total += round.investment}
+    end
+    def investors
+      funding_rounds.map{|round| round.venture_capitalist}.uniq
+    end
+    def big_investors
+      investors.select{|investor| VentureCapitalist.tres_commas_club.include? (investor)}
+    end
+  end
