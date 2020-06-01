e820b9c648193bc5e00c4ffaea8e5b48f5ff054d
mlkelly <maurak87@gmail.com>
2020-06-01 15:06:19 -0400

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,43 @@
 class VentureCapitalist
+    @@all = [] 
+    def initialize(name, total_worth)
+        @name = name 
+        @total_worth = total_worth 
+        self.class.all << self 
+    end 
+    attr_accessor :name, :total_worth 
+    def self.all 
+        @@all 
+    end 
 
-end
+    def self.tres_commas_club
+        self.all.select {|venture_firm| venture_firm.total_worth > 1000000000}
+    end 
+
+    def tres_helper 
+        if self.class.tres_commas_club.find {|firm| firm == self} 
+            return true 
+        else return false 
+        end 
+    end 
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select {|fund_round| fund_round.venture_capitalist == self}
+    end
+
+    def portfolio
+        funding_rounds.map {|fund_round| fund_round.startup}.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.max {|fund_round| fund_round.investment}
+    end
+
+    def invested(domain)
+        funding_rounds.select {|fund_round| fund_round.startup.domain == domain}.max {|fund_round| fund_round.investment}
+    end
+end 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#startup (founder, name, domain)
+s1= Startup.new("Alexis", "Smelly Ventures", "domain1")
+s2= Startup.new("Maura", "I'm Hungry", "domain2")
+
+v1 = VentureCapitalist.new("AlexisTeh", 50000)
+v2 = VentureCapitalist.new("MauraKelly", 60000000000000)
+v3 = VentureCapitalist.new("Anon", 70000000000000)
+
+# (startup, venture_capitalist, type, investment)
+f1 = FundingRound.new(s1, v1, "type1", 100)
+f2 = FundingRound.new(s1, v2, "type2", 1000000)
+f3 = FundingRound.new(s1, v3, "type3", 11232353)
+
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+0 #leave this here to ensure binding.pry isn't the last line 
