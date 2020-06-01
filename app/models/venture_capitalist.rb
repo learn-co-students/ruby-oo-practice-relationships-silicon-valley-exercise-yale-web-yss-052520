d683fe325b4a183a0620bc8bb16fcbac029569d7
Dennis <Dennis.portillo@yale.edu>
2020-06-01 12:51:18 -0700

    done

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,62 @@
 class VentureCapitalist
+    attr_accessor :name , :total_worth
+    @@all =[]
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        VentureCapitalist.all.select {|vc| vc.total_worth > 1000000000}
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select {|f| f.venture_capitalist == self}
+    end
+    
+    def portfolio
+        funding_rounds.map {|f| f.startup}.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.max {|f| f.investment}
+    end
+
+    def domainFinder(domain_name)
+        funding_rounds.select {|f| f.startup.domain == domain_name}
+    end
+
+    def invested(domain_name)
+        domainFinder(domain_name).sum{|x| x.investment}
+    end
 
 end
+
+
+# `VentureCapitalist#offer_contract`
+#   - given a **startup object**, type of investment 
+#   (as a string), and the amount invested (as a float), 
+#   creates a new funding round and associates it with 
+#   that startup and venture capitalist.
+# - `VentureCapitalist#funding_rounds`
+#   - returns an array of all funding rounds for that 
+#   venture capitalist
+# - `VentureCapitalist#portfolio`
+#   - Returns a **unique** list of all startups this 
+#   venture capitalist has funded
+# - `VentureCapitalist#biggest_investment`
+#   - returns the largest funding round given by this 
+#   venture capitalist
+# - `VentureCapitalist#invested`
+#   - given a **domain string**, returns the total amount 
+#   invested in that domain
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+s1 = Startup.new("Google", "Founder", "domain")
+s2 = Startup.new("facebook", "Mark", "domain")
+s3 = Startup.new("Microsoft", "Bill", "dmns")
+
+v1 = VentureCapitalist.new("Jeff", 10000000000)
+v2 = VentureCapitalist.new("Sam", 10)
+v3 = VentureCapitalist.new("Alex", 12345)
+
+f1 = FundingRound.new(s1,v1,"Angel",100)
+f2 = FundingRound.new(s1,v2,"Series A",200)
+f3 = FundingRound.new(s2,v3,"Series B",20)
+f4 = FundingRound.new(s2,v3,"Series C",10)
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
